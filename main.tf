17b31e8acdfd0c5437c20e5807cd48a125a4ff98
dejair-tech <dejair.santos@hotmail.com>
2021-08-27 17:32:07 -0300

    Adicionado Bloco de Redes

--- main.tf
+++ main.tf
@@ -4,6 +4,9 @@ resource "google_storage_bucket_object" "learner_id_file" {
   bucket = "cloud-journey"
 }
 
-resource "google_compute_network" "learner" {
-  // TODO Adicionar e documentar aqui os campos necessários para criar a rede
+resource "google_compute_network" "vpc_network" {
+  #Definição do nome da VPC
+  name = "tabajaras-network" 
 }
+
+
